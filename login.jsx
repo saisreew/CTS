0224d648a6c84faec166a5672953d27f6d75a3a0
saisreew <47133815+saisreew@users.noreply.github.com>
2020-03-17 13:04:52 +0530

    Add files via upload

---
+++ login.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class Login extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1 className="text-center">Login page</h1>
+      </div>
+    );
+  }
+}
+
+export default Login;
